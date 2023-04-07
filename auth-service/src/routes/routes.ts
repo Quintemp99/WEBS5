9fa7b3936e0422e9d41688c4f7bdba893c580772
Yvan de Wert <ydewert@gmail.com>
2023-04-07 22:26:18 +0200

    fixed dev

--- auth-service/src/routes/routes.ts
+++ auth-service/src/routes/routes.ts
@@ -6,36 +6,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default ({ app }: TRoutesInput) => {
-
-    app.post('/login', (req,res, next) =>{
-        passport.authenticate(
-            'login',
-            async (err:any, user:any, info:any) => {
-              try {
-                if (err || !user) {
-                  return next(`An error occurred: ${info.message}`);
-                }
-      
-                req.login(
-                  user,
-                  { session: false },
-                  async (error) => {
-                    if (error) return next(error);
-      
-                    const body = { _id: user._id, email: user.email, roles: user.roles };
-                    const token = jwt.sign({ user: body }, 
-                      process.env.JWT_SECRET || "", 
-                      { expiresIn: "1h"});
-      
-                    return res.json({ token });
-                  }
-                );
-              } catch (error) {
-                return next(error);
-              }
-            }
-          )(req, res, next);
-    })
+  app.post("/login", (req, res, next) => {
+    passport.authenticate("login", async (err: any, user: any, info: any) => {
+      try {
+        if (err || !user) {
+          return next(`An error occurred: ${info.message}`);
+        }
 
         req.login(user, { session: false }, async (error) => {
           if (error) return next(error);
